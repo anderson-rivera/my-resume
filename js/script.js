530076e5827b90ee2c166f8ceeec05bfe8b4abe2
afrm29 <anderson.rivera@interaction-interns.com>
2021-01-18 15:06:38 -0600

    add button go  to top

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//js of site
+//btn go to top
+const btnUp = document.getElementById('gtop');
+
+//for detect when user make scroll 
+window.onscroll = function(){
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+        btnUp.style.display = 'block';
+    }else{
+        btnUp.style.display = 'none';
+    }
+};
+
+//function for click bnt-top
+btnUp.addEventListener('click' , up_function);
+function up_function(){
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+};
